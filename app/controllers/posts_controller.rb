49d4f8e92e7edab679143e3f05f66f21146bc175
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-06 18:03:11 +0700

    push my_blog crud, login, register

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]  # Kiểm tra quyền sở hữu bài viết
 
   def index
     @posts = Post.all
@@ -10,19 +11,21 @@ class PostsController < ApplicationController
   end
 
   def create
-  @post = current_user.posts.build(post_params) # Gán user_id từ current_user
+    @post = current_user.posts.build(post_params)  # Gán user_id từ current_user
 
-  if @post.save
-    redirect_to posts_path, notice: 'Post was successfully created.'  # Chuyển hướng về trang danh sách
-  else
-    render :new
-  end
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
+    # Bài viết sẽ được hiển thị cho tất cả người dùng
   end
 
   def edit
+    # Phương thức này sẽ chỉ được thực hiện nếu người dùng là chủ sở hữu của bài viết
   end
 
   def update
@@ -41,10 +44,17 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:id])  # Lấy bài viết theo ID
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :category_id, :published)
+    params.require(:post).permit(:title, :content, :category_id, :published)  # Các tham số cho bài viết
+  end
+
+  def authorize_user
+    # Kiểm tra nếu bài viết thuộc về người dùng hiện tại hay không
+    unless @post.user_id == current_user.id
+      redirect_to posts_path, alert: "Bạn không có quyền chỉnh sửa hoặc xóa bài viết này."
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :posts
   has_many :posts, dependent: :destroy
-
+  
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
