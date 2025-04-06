dac5eaade54f47b722981617be4c4c94ce7d95a7
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-06 18:08:09 +0700

    main

--- app/controllers/profile_controller.rb
+++
@@ -1,5 +0,0 @@
-class ProfileController < ApplicationController
-  def index
-        @user = current_user
-  end
-end
--- db/migrate/20250404083422_create_member.rb
+++
@@ -1,9 +0,0 @@
-class CreateMember < ActiveRecord::Migration[7.2]
-  def change
-    create_table :members do |t|
-      t.string :name
-      t.string :email
-      t.timestamps
-    end
-  end
-end
